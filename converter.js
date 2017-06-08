a1728a3bcba190afcb95854aadc290850e9ad51a
An-AngryBear <monahajt@gmail.com>
2017-06-08 09:28:27 -0500

    switch js from html to jsfile, final touches

--- converter.js
+++ converter.js
@@ -1,23 +1,57 @@
+
+// Celsius to fahrenheit function
 function celsToFahr(celsTemp) {
 	return ((celsTemp * 1.8) + 32).toFixed(1);
 }
 
+// Fahrenheit to celsius function
 function fahrToCels(fahrTemp) {
 	return ((fahrTemp - 32) * (5/9)).toFixed(1);
 }
 
+// DOM elements
 const tempDisplay = document.getElementById("temp-display"),
 	fahrenheitRadio = document.getElementById("fahr"),
 	celsiusRadio = document.getElementById("cels"),
-	userInput = document.getElementById("entryfield");
+	userInput = document.getElementById("entryfield"),
+	submitButton = document.getElementById('submit-button'),
+	resetButton = document.getElementById("reset-button");
 
+// Converts user input values with corresponding colors
 function converter() {
 	if (celsiusRadio.checked) {
-		tempDisplay.innerHTML = fahrToCels(userInput.value) + " degrees!";
+		tempDisplay.innerHTML = "<span id='output'>" + fahrToCels(userInput.value) + "</span> degrees!";
 	} else {
-		tempDisplay.innerHTML = celsToFahr(userInput.value) + " degrees!";
+		tempDisplay.innerHTML = "<span id='output'>" + celsToFahr(userInput.value) + "</span> degrees!";
+	}
+	colorSpan();
+}
+// Enter on Submit
+function enterAsSubmit() {
+	if (event.keyCode == 13) {
+		submitButton.click();
+		event.preventDefault();
 	}
 }
-function reset() {
-	tempDisplay.innerHTML = "----";
+// Function changes neutral temps to green, cold temps to blue, and hot temps to red
+function colorSpan() {
+	tempDisplay.classList = "";
+	let output = document.getElementById("output");
+	if((parseInt(output.innerHTML) > 90 && fahrenheitRadio.checked) || (parseInt(output.innerHTML) > 32 && celsiusRadio.checked)) {
+		tempDisplay.classList.add("hot");
+	} else if((parseInt(output.innerHTML) <= 32 && fahrenheitRadio.checked) || (parseInt(output.innerHTML) <= 0 && celsiusRadio.checked)) {
+		tempDisplay.classList.add("cold");
+	} else {
+		tempDisplay.classList.add("warm-cool");
+	}
 }
+
+// Event listeners for submit button, enter key, and reset button
+userInput.addEventListener("keydown", enterAsSubmit);
+
+submitButton.addEventListener("click", converter);
+
+resetButton.addEventListener("click", function() {
+	tempDisplay.innerHTML = " ";
+});
+
